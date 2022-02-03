e20aef92b07da85bf5083a42f032cc53469fb697
Krzysztof Hermaszewski <hermaszewski.krzysztof@gmail.com>
2022-02-03 20:04:31 +0100

    Add File Upload

--- BulkuBookWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkuBookWeb/Areas/Admin/Controllers/ProductController.cs
@@ -12,10 +12,11 @@ namespace BulkyBookWeb.Controllers
     public class ProductController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
-
-        public ProductController(IUnitOfWork unitOfWork)
+        private readonly IWebHostEnvironment _hostEnvironment;
+        public ProductController(IUnitOfWork unitOfWork, IWebHostEnvironment hostEnvironment)
         {
             _unitOfWork = unitOfWork;
+            _hostEnvironment = hostEnvironment;
         }
 
         public IActionResult Index()
@@ -53,23 +54,54 @@ namespace BulkyBookWeb.Controllers
             }
             else
             {
-                //Update product
+                productVM.Product = _unitOfWork.Product.GetFirstOrDefault(u => u.Id == id);
+                return View(productVM);
             }
             
-                
-            return View(productVM);
+               
         }
 
         //POST
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Upsert(CoverType obj, IFormFile file)
+        public IActionResult Upsert(ProductVM obj, IFormFile file)
         {
             if (ModelState.IsValid)
             {
-                _unitOfWork.CoverType.Update(obj);
+                string wwwRootPath = _hostEnvironment.WebRootPath;
+                if (file != null)
+                {
+                    string fileName = Guid.NewGuid().ToString();
+                    var uploads = Path.Combine(wwwRootPath, @"images\products");
+                    var extension = Path.GetExtension(file.FileName);
+
+                    //if (obj.Product.ImageUrl != null)
+                    //{
+                    //    var oldImagePath = Path.Combine(wwwRootPath, obj.Product.ImageUrl.TrimStart('\\'));
+                    //    if (System.IO.File.Exists(oldImagePath))
+                    //    {
+                    //        System.IO.File.Delete(oldImagePath);
+                    //    }
+                    //}
+
+                    using (var fileStreams = new FileStream(Path.Combine(uploads, fileName + extension), FileMode.Create))
+                    {
+                        file.CopyTo(fileStreams);
+                    }
+                    obj.Product.ImageUrl = @"\images\products\" + fileName + extension;
+
+                }
+                
+                if (obj.Product.Id == 0)
+                {
+                    _unitOfWork.Product.Add(obj.Product);
+                }
+                else
+                {
+                    _unitOfWork.Product.Update(obj.Product);
+                }
                 _unitOfWork.Save();
-                TempData["success"] = "Cover Type updated successfully";
+                TempData["success"] = "Product created successfully";
                 return RedirectToAction("Index");
             }
             return View(obj);
--- BulkyBook.DataAccess/Repository/ProductRepository.cs
+++ BulkyBook.DataAccess/Repository/ProductRepository.cs
@@ -3,7 +3,6 @@ using BulkyBook.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,30 +10,32 @@ namespace BulkyBook.DataAccess.Repository
 {
     public class ProductRepository : Repository<Product>, IProductRepository
     {
-        private readonly ApplicationDbContext _db;
+        private ApplicationDbContext _db;
+
         public ProductRepository(ApplicationDbContext db) : base(db)
         {
             _db = db;
         }
 
+
         public void Update(Product obj)
         {
-            var objFromDb = _db.Products.FirstOrDefault(x => x.Id == obj.Id);
-            if(objFromDb != null)
+            var objFromDb = _db.Products.FirstOrDefault(u => u.Id == obj.Id);
+            if (objFromDb != null)
             {
                 objFromDb.Title = obj.Title;
-                objFromDb.Description = obj.Description;
+                objFromDb.ISBN = obj.ISBN;
                 objFromDb.Price = obj.Price;
                 objFromDb.Price50 = obj.Price50;
-                objFromDb.Price100 = obj.Price100;
                 objFromDb.ListPrice = obj.ListPrice;
-                objFromDb.ISBN = obj.ISBN;
-                objFromDb.Author = obj.Author;
+                objFromDb.Price100 = obj.Price100;
+                objFromDb.Description = obj.Description;
                 objFromDb.CategoryId = obj.CategoryId;
+                objFromDb.Author = obj.Author;
                 objFromDb.CoverTypeId = obj.CoverTypeId;
-                if(obj.ImageUrl != null)
+                if (obj.ImageUrl != null)
                 {
-                    obj.ImageUrl = obj.ImageUrl;
+                    objFromDb.ImageUrl = obj.ImageUrl;
                 }
             }
         }
