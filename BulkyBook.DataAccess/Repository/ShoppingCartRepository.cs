1335df1a7a5838880ce9181c9a4594b7b0c951ac
Krzysztof Hermaszewski <hermaszewski.krzysztof@gmail.com>
2022-02-09 19:57:42 +0100

    Shopping Cart Controller

--- BulkyBook.DataAccess/Repository/ShoppingCartRepository.cs
+++ BulkyBook.DataAccess/Repository/ShoppingCartRepository.cs
@@ -15,6 +15,18 @@ namespace BulkyBook.DataAccess.Repository
         public ShoppingCartRepository(ApplicationDbContext db) : base(db)
         {
             _db = db;
-        }   
+        }
+
+        public int DecrementCount(ShoppingCart shoppingCart, int count)
+        {
+            shoppingCart.Count -= count;
+            return shoppingCart.Count;
+        }
+
+        public int IncrementCount(ShoppingCart shoppingCart, int count)
+        {
+            shoppingCart.Count += count;
+            return shoppingCart.Count;
+        }
     }
 }
