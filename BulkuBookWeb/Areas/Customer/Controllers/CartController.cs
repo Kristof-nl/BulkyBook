d5bb5c3684498414a9c2cf728fb608ec1aba5c44
Krzysztof Hermaszewski <hermaszewski.krzysztof@gmail.com>
2022-02-10 14:38:10 +0100

    Shopping Cart total price

--- BulkuBookWeb/Areas/Customer/Controllers/CartController.cs
+++ BulkuBookWeb/Areas/Customer/Controllers/CartController.cs
@@ -12,6 +12,7 @@ namespace BulkyBookWeb.Areas.Customer.Controllers
     {
         private readonly IUnitOfWork _unitOfWork;
         public ShoppingCartVM ShoppingCartVM { get; set; }
+        public int OrderTotal { get; set; }
         public CartController(IUnitOfWork unitOfWork)
         {
             _unitOfWork = unitOfWork;
@@ -27,8 +28,29 @@ namespace BulkyBookWeb.Areas.Customer.Controllers
                 ListCart = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == claim.Value,
                 includeProperties: "Product")
             };
+            foreach(var cart in ShoppingCartVM.ListCart)
+            {
+                cart.Price = GetPriceBaseOnQuantity(cart.Count, cart.Product.Price, cart.Product.Price50, cart.Product.Price100);
+                ShoppingCartVM.CartTotal += (cart.Price * cart.Count);
+            }
 
             return View(ShoppingCartVM);
+        }
+
+        private double GetPriceBaseOnQuantity(double quantity, double price, double price50, double price100)
+        {
+            if (quantity <= 50)
+            {
+                return price;
+            }
+            else
+            {
+                if (quantity <= 100)
+                {
+                    return price50;
+                }
+                return price100;
+            }
         }
     }
 }
